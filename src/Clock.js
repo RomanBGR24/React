5b3331ea6f90abfc647ad318fa48d77c1008ae9c
RomanBGR24 <62135606+RomanBGR24@users.noreply.github.com>
2021-09-23 10:49:01 +0300

    Add element & state. Fix 1

--- src/Clock.js
+++ src/Clock.js
@@ -2,11 +2,16 @@ import React, {Component} from "react";
 
 class Clock extends Component {
     constructor(props) {
+        super(props);
 
     }
 
     render() {
-        
+        return(
+            <div>
+                
+            </div>
+        );
     }
 }
 
