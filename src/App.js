04c5683cf14362b22b4c55005cff22c726fdcedd
RomanBGR24 <62135606+RomanBGR24@users.noreply.github.com>
2021-09-23 10:39:13 +0300

    add element component and its state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import Element from './Element';
+import Clock from './Clock';
 
 class App extends Component {
   render() {
@@ -14,6 +16,10 @@ class App extends Component {
     
     const child_text = this.props.children;
 
+    let array_of_elements = [];
+    for (let i = 0; i < 5; i++){
+      array_of_elements.push(<Element />);
+    }
     return (
       <div >
         <h1> My headers </h1>
@@ -21,6 +27,8 @@ class App extends Component {
         <br/>
         <br/>
         <p> And here some text from children: {child_text} </p>
+            {array_of_elements}
+
       </div>
     );
   }
